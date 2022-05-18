7b5972acef4b44a7748647e418ecbb837acde1e1
hacan359 <59626486+hacan359@users.noreply.github.com>
2022-05-18 15:31:56 +0300

    namespace missing

--- src/DynamicFormWidget.php
+++ src/DynamicFormWidget.php
@@ -5,7 +5,7 @@
  * @license   https://github.com/bajadev/yii2-dynamicform/blob/master/LICENSE
  */
 
-namespace kidzen\dynamicform;
+namespace wbraganca\dynamicform;
 
 use Symfony\Component\CssSelector\CssSelectorConverter;
 use Yii;
